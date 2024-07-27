cef217a55336a50a278d37fee12a0e862e3d8b9e
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-07-28 01:52:48 +0600

    create language model

---
+++ MultiLanguageApp/Models/Language.cs
@@ -0,0 +1,11 @@
+﻿namespace MultiLanguageApp.Models
+{
+    public class Language
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int UsedInCountry { get; set; }
+        public int Lifetime { get; set; }
+        public string Continent { get; set; }
+    }
+}
